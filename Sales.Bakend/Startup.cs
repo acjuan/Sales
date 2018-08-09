c5c45e6a8047d45ac63028104e520163f56c8ccf
juan alberto <acjuan@live.com>
2018-08-08 22:56:40 -0300

    inicio projecto sales

---
+++ Sales.Bakend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sales.Bakend.Startup))]
+namespace Sales.Bakend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
